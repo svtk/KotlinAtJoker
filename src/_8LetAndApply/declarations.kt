09e1308b276dd0dd0bfb9e0b4c49d1a6496fdd7f
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-04-05 14:21:54 +0300

    An example with "let" and "apply" replaced with a more illustrative one ("with")

--- src/_8LetAndApply/declarations.kt
+++
@@ -1,8 +0,0 @@
-package _8LetAndApply.declarations
-
-
-inline fun <T, R> T.let(f: (T) -> R): R = f(this)
-
-
-inline fun <T> T.apply(f: T.() -> Unit): T { f(); return this }
-
---
+++ src/_8LetAndApply/withIsTheFunction.kt
@@ -0,0 +1,16 @@
+package _8LetAndApply
+
+fun getAlphabetInString(): String {
+    val sb = StringBuilder()
+    with (sb) {
+        this.append("Alphabet: ")
+        for (ch in 'a'..'z') {
+            append(ch)
+        }
+    }
+    return sb.toString()
+}
+
+fun main(args: Array<String>) {
+    println(getAlphabetInString())
+}
